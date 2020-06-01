74667e29242f1be1bcb10d95de226da4d5d48884
Viplava Bhardwaj <viplavabhardwaj007@gmail.com>
2020-06-01 16:56:34 +0530

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import { Text, FlatList, ScrollView} from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
-import {LEADERS} from '../shared/leaders';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
 
-
+const mapStateToProps = state => {
+    return {
+      leaders: state.leaders
+    }
+  }
 
 class About extends Component{
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            leaders: LEADERS
-        };
-    }
 
     static navigationOptions = {
         title: 'About Us',
@@ -38,7 +37,7 @@ class About extends Component{
                         title={item.name}
                         subtitle={item.description}
                         hideChevron={true}
-                        leftAvatar={{ source: require('./images/alberto.png')}}
+                        leftAvatar={{ source:  {uri: baseUrl + item.image}}}
                     />
             );
         }
@@ -50,7 +49,7 @@ class About extends Component{
                     title='Corporate Leadership'
                     >
                     <FlatList 
-                        data={this.state.leaders}
+                        data={this.props.leaders.leaders}
                         renderItem={renderLeader}
                     />
                 </Card>
@@ -59,4 +58,4 @@ class About extends Component{
     }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
