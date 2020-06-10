67392d3184bebfabc6ac03674f283c662f6af922
Viplava Bhardwaj <viplavabhardwaj007@gmail.com>
2020-06-10 16:01:24 +0530

    Social Sharing

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import { View, Text,ScrollView, FlatList,Modal,StyleSheet,TouchableHighlight, Button, Alert, PanResponder } from 'react-native';
+import { View, Text,ScrollView, FlatList,Modal,StyleSheet,TouchableHighlight, Share, Alert, PanResponder } from 'react-native';
 import { Card,Icon,Input } from 'react-native-elements';
 import {connect } from 'react-redux';
 import {baseUrl} from '../shared/baseUrl';
@@ -64,6 +64,16 @@ function RenderDish(props) {
         }
     })
 
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        })
+    }
+
     if(dish != null) {
         return (
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
@@ -96,6 +106,15 @@ function RenderDish(props) {
                             type= 'font-awesome'
                             color= '#512DA8'
                             onPress={() => props.toggleModal()}
+                            />
+                        <Icon
+                            raised
+                            reverse
+                            name='share'
+                            type='font-awesome'
+                            color='#51D2A8'
+                            style={styles.cardItem}
+                            onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} 
                             />
                     </View>
                 </Card>
